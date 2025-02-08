74ca346ccb5dda8c352f3f8b48772c31cc6d4d6d
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2025-02-08 20:48:42 +0200

    feat: immproved validation schema

--- src/validation/auth/loginValidationSchema.ts
+++ src/validation/auth/loginValidationSchema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const loginRequestSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1),
-});
+export const loginRequestSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(1),
+  })
+  .strict();
